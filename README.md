809b1de71274baaa49a6975130bca9606b2bf045
Mustafa Can Yucel <m.can.yucel.1@gmail.com>
2023-07-06 17:00:12 +0300

    add sanitization and license

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # autoupdater-server
 autoupdater-server
+
+Uses [kennygrant/sanitize](https://github.com/kennygrant/sanitize) to sanitize the input (See license).
--- autoupdater.go
+++ autoupdater.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"html"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func loadData(appName string) (string, error) {
@@ -99,8 +103,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		data := map[string]interface{}{
-			"version": appVersion,
-			"url":     downloadUrl,
+			"Url":     downloadUrl,
+			"Version": appVersion,
 		}
 		jsonData, err := json.Marshal(data)
 		if err != nil {
@@ -119,4 +123,48 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func sanitizeHTML(s string) (output string) {
+	if !strings.ContainsAny(s, "<>") {
+		output = s
+	} else {
+		s = strings.Replace(s, "\n", "", -1)
+		s = strings.Replace(s, "</p>", "\n", -1)
+		s = strings.Replace(s, "<br>", "\n", -1)
+		s = strings.Replace(s, "</br>", "\n", -1)
+		s = strings.Replace(s, "<br/>", "\n", -1)
+		s = strings.Replace(s, "<br />", "\n", -1)
+
+		b := bytes.NewBufferString("")
+		inTag := false
+		for _, r := range s {
+			switch r {
+			case '<':
+				inTag = true
+			case '>':
+				inTag = false
+			default:
+				if !inTag {
+					b.WriteRune(r)
+				}
+			}
+		}
+		output = b.String()
+	}
+	output = strings.Replace(output, "&#8216;", "'", -1)
+	output = strings.Replace(output, "&#8217;", "'", -1)
+	output = strings.Replace(output, "&#8220;", "\"", -1)
+	output = strings.Replace(output, "&#8221;", "\"", -1)
+	output = strings.Replace(output, "&nbsp;", " ", -1)
+	output = strings.Replace(output, "&quot;", "\"", -1)
+	output = strings.Replace(output, "&apos;", "'", -1)
+	output = html.UnescapeString(output)
+	output = template.HTMLEscapeString(output)
+	output = strings.Replace(output, "&#34;", "\"", -1)
+	output = strings.Replace(output, "&#39;", "'", -1)
+	output = strings.Replace(output, "&amp; ", "& ", -1)
+	output = strings.Replace(output, "&amp;amp; ", "& ", -1)
+
+	return output
 }
